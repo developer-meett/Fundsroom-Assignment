3cb89b1c21e314b7d7e0307dacefba9d37b12c4c
Meet Suhagiya <meetsuhagiya@Meets-MacBook-Air.local>
2026-08-11 19:14:56 +0000

    Wrap React Router paths with role protection to secure endpoints

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import { RoleRoute } from './components/RoleRoute';
 import { Layout } from './components/Layout';
 import { Login } from './pages/Login';
 import { Dashboard } from './pages/Dashboard';
@@ -21,21 +22,46 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
+          
           <Route element={<ProtectedRoute />}>
             <Route element={<Layout />}>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/customers" element={<CustomerList />} />
-              <Route path="/customers/new" element={<CustomerForm />} />
-              <Route path="/customers/:id" element={<CustomerDetail />} />
-              <Route path="/customers/:id/edit" element={<CustomerForm />} />
-              <Route path="/products" element={<ProductList />} />
-              <Route path="/products/new" element={<ProductForm />} />
-              <Route path="/products/:id" element={<ProductDetail />} />
-              <Route path="/products/:id/edit" element={<ProductForm />} />
-              <Route path="/inventory" element={<ProductList />} />
-              <Route path="/challans" element={<ChallanList />} />
-              <Route path="/challans/new" element={<ChallanForm />} />
-              <Route path="/challans/:id" element={<ChallanDetail />} />
+              
+              {/* Customers (Viewable by ADMIN, SALES, ACCOUNTS) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'SALES', 'ACCOUNTS']} />}>
+                <Route path="/customers" element={<CustomerList />} />
+                <Route path="/customers/:id" element={<CustomerDetail />} />
+              </Route>
+              
+              {/* Customer Editing (Only ADMIN, SALES) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'SALES']} />}>
+                <Route path="/customers/new" element={<CustomerForm />} />
+                <Route path="/customers/:id/edit" element={<CustomerForm />} />
+              </Route>
+              
+              {/* Products (Viewable by All) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'WAREHOUSE', 'SALES', 'ACCOUNTS']} />}>
+                <Route path="/products" element={<ProductList />} />
+                <Route path="/products/:id" element={<ProductDetail />} />
+                <Route path="/inventory" element={<ProductList />} />
+              </Route>
+              
+              {/* Product Editing (Only ADMIN, WAREHOUSE) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'WAREHOUSE']} />}>
+                <Route path="/products/new" element={<ProductForm />} />
+                <Route path="/products/:id/edit" element={<ProductForm />} />
+              </Route>
+              
+              {/* Challans (Viewable by All) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'SALES', 'WAREHOUSE', 'ACCOUNTS']} />}>
+                <Route path="/challans" element={<ChallanList />} />
+                <Route path="/challans/:id" element={<ChallanDetail />} />
+              </Route>
+              
+              {/* Challan Creation (Only ADMIN, SALES) */}
+              <Route element={<RoleRoute allowedRoles={['ADMIN', 'SALES']} />}>
+                <Route path="/challans/new" element={<ChallanForm />} />
+              </Route>
             </Route>
           </Route>
         </Routes>
